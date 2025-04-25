ad63f5c9cc9db06ffa0f7902004d9b3d42a0c357
haroon <haroonurrasheed1212@gmail.com>
2025-04-25 19:27:12 +0500

    Initial comit message for this project

---
+++ src/main/java/com/restfullApi/ProductLists/Products/RestFulApiProductApplication.java
@@ -0,0 +1,14 @@
+package com.restfullApi.ProductLists.Products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulApiProductApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestFulApiProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restfullApi/ProductLists/Products/RestFulApiProductListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfullApi.ProductLists.Products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulApiProductListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
